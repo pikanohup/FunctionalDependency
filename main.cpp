528511fc2f3ec3b1c4e5f959c6bb18126777cd18
Pikachu <pocketfish@yeah.net>
2018-05-22 20:59:40 +0800

    modify IO

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 #include "tane.h"
 
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-	  std::cout << "ERROR: Not enough arguments" << std::endl;
+  if (argc < 4) {
+	  std::cout << "ERROR: Not enough arguments\nUSAGE: ./tane input_file output_file attr_num\n";
+    
     return 1;
   }
 
   clock_t start = clock();
 
-  Tane tane(argv[1], 15);
+  Tane tane(argv[1], atoi(argv[3]));
   tane.ComputeDependencies();
   tane.Output(argv[2]);
 
